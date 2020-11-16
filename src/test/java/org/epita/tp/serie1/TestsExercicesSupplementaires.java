f3b5c2a3543e4afbfbc00c5f744c60d35e9b1c4d
Romain NOEL <romain.d.noel@bnpparibas.com>
2020-11-16 11:19:50 +0100

    Changing test namings

--- src/test/java/org/epita/tp/serie1/TestsExercicesSupplementaires.java
+++ src/test/java/org/epita/tp/serie1/TestsExercicesSupplementaires.java
@@ -8,57 +8,57 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TestsExercicesSupplementaires {
 
     @Test
-    public void exercice1(){
+    public void test_exercice1(){
 
     }
 
     @Test
-    public void exercice2(){
+    public void test_exercice2(){
 
     }
 
     @Test
-    public void exercice3(){
+    public void test_exercice3(){
 
     }
 
     @Test
-    public void exercice4(){
+    public void test_exercice4(){
 
     }
 
     @Test
-    public void exercice5(){
+    public void test_exercice5(){
 
     }
 
     @Test
-    public void exercice6(){
+    public void test_exercice6(){
 
     }
 
     @Test
-    public void exercice7(){
+    public void test_exercice7(){
 
     }
 
     @Test
-    public void exercice8(){
+    public void test_exercice8(){
 
     }
 
     @Test
-    public void exercice9(){
+    public void test_exercice9(){
 
     }
 
     @Test
-    public void exercice10(){
+    public void test_exercice10(){
 
     }
 
     @Test
-    public void exercice11(){
+    public void test_exercice11(){
 
     }
     
