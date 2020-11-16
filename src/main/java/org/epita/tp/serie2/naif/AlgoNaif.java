62e48f40ce93215ddd060d2c7475b3e37618db58
Romain NOEL <romain.d.noel@bnpparibas.com>
2020-11-16 18:27:08 +0100

    Ajout de la correction de l'algo Naif

--- src/main/java/org/epita/tp/serie2/naif/AlgoNaif.java
+++ src/main/java/org/epita/tp/serie2/naif/AlgoNaif.java
@@ -1,61 +1,177 @@
 package org.epita.tp.serie2.naif;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AlgoNaif {
 
-    private /*final*/ int taillePlateau;
-    private /*final*/ boolean[][] plateau;
+    private final int taillePlateau;
+    private final boolean[][] plateau;
 
     public AlgoNaif(int taillePlateau) {
+        this.taillePlateau = taillePlateau;
+        plateau = new boolean[taillePlateau][taillePlateau];
+        for (int colonne = 0; colonne < taillePlateau; colonne++) {
+            plateau[0][colonne] = true;
+        }
     }
 
     public AlgoNaif(boolean[][] plateau) {
+        this.taillePlateau = plateau.length;
+        this.plateau = plateau;
     }
 
     public boolean[][] getPlateau() {
-        return plateau;
+        return copierPlateau(plateau);
     }
 
-    public List<boolean[][]> listerSolutionsNaif(){
-        return null;
+    public List<boolean[][]> listerSolutions(){
+        List<boolean[][]> solutions = new ArrayList<>();
+        boolean aEncoreDesSolutionsPossible = true;
+
+        while(aEncoreDesSolutionsPossible){
+            try {
+                if(estUneSolution()){
+                    boolean[][] solution = copierPlateau(plateau);
+                    solutions.add(solution);
+                }
+                coupSuivant();
+            } catch (Exception e) {
+                aEncoreDesSolutionsPossible = false;
+            }
+        }
+        return solutions;
     }
 
-    public void coupSuivant() {
-        return;
+    public void coupSuivant() throws Exception {
+        coupSuivantColonne(0);
     }
 
-    private boolean estUneSolution() {
+    private void coupSuivantColonne(int colonne) throws Exception {
+        boolean cycleComplet = avancerLigne(colonne);
+        if(cycleComplet){
+            if(colonne+1 >= taillePlateau){
+                throw new Exception("Vous avez épuisé toutes les possibilités");
+            }
+            coupSuivantColonne(colonne+1);
+        }
+    }
+
+    private boolean avancerLigne(int colonne) {
+        boolean damePresente = plateau[plateau.length-1][colonne];
+        if(damePresente){
+            plateau[plateau.length-1][colonne] = false;
+            plateau[0][colonne] = true;
+            return true;
+        }
+
+        for (int ligne = 0; ligne < plateau.length - 1; ligne++) {
+            damePresente = plateau[ligne][colonne];
+            if(damePresente){
+                plateau[ligne][colonne] = false;
+                plateau[ligne+1][colonne] = true;
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean estUneSolution() {
+        for (int ligne = 0; ligne < plateau.length; ligne++) {
+            for (int colonne = 0; colonne < plateau[ligne].length; colonne++) {
+                if(plateau[ligne][colonne]){
+                    boolean etatOk = verifierLigne(colonne);
+                    etatOk &= verifierColonne(ligne);
+                    etatOk &= verifierDiagonaleDroite(colonne - ligne);
+                    etatOk &= verifierDiagonaleGauche(colonne + ligne);
+                    if(!etatOk){
+                        return false;
+                    }
+                }
+            }
+        }
         return true;
     }
 
-    private boolean verifierColonne(int colonne) {
-        return true;
+    public boolean verifierColonne(int colonne) {
+        int nombreDeDames = 0;
+        for (int ligne = 0; ligne < plateau[colonne].length; ligne++) {
+            if(plateau[ligne][colonne]){
+                nombreDeDames++;
+            }
+        }
+        return nombreDeDames<2;
     }
 
-    private boolean verifierLigne(int ligne) {
-        return true;
+    public boolean verifierLigne(int ligne) {
+        int nombreDeDames = 0;
+        for (int colonne = 0; colonne < plateau.length; colonne++) {
+            if(plateau[ligne][colonne]){
+                nombreDeDames++;
+            }
+        }
+        return nombreDeDames<2;
     }
 
     public boolean verifierDiagonaleDroite(int diagonaleDroite) {
-        return true;
+        int nombreDeDames = 0;
+        for (int ligne = 0; ligne < plateau.length; ligne++) {
+            if(diagonaleDroite+ligne>=0 && diagonaleDroite+ligne<taillePlateau){
+                int colonne = diagonaleDroite+ligne;
+                if(plateau[ligne][colonne]){
+                    nombreDeDames++;
+                }
+            }
+        }
+        return nombreDeDames<2;
     }
 
     public boolean verifierDiagonaleGauche(int diagonaleGauche) {
-        return true;
+        int nombreDeDames = 0;
+        for (int ligne = 0; ligne < plateau.length; ligne++) {
+            if(diagonaleGauche-ligne>=0 && diagonaleGauche-ligne<taillePlateau){
+                int colonne = diagonaleGauche-ligne;
+                if(plateau[ligne][colonne]){
+                    nombreDeDames++;
+                }
+            }
+        }
+        return nombreDeDames<2;
     }
 
     public static boolean[][] copierPlateau(boolean [][] plateau) {
-        return null;
+        //Ici on peut utilise un plateau.clone(), mais cela ne fonctionne pas sur les objets sous-jacents
+        //Il faudrait une option de deepCopy.
+        boolean [][] copy = new boolean[plateau.length][plateau[0].length];
+        for (int i = 0; i < plateau.length; i++) {
+            //Ceci dit, on pourrait utiliser un clone() à la place de cette boucle
+            for (int j = 0; j < plateau[i].length; j++) {
+                copy[i][j] = plateau[i][j];
+            }
+        }
+        return copy;
     }
 
     public static String plateauToString(boolean[][] plateau){
-        return "";
+        String representation = "";
+        for (int ligne = 0; ligne < plateau.length; ligne++) {
+            for (int colonne = 0; colonne < plateau[ligne].length; colonne++) {
+                boolean damePresente = plateau[ligne][colonne];
+                representation += "|";
+                if(damePresente){
+                    representation += "X";
+                }else{
+                    representation += " ";
+                }
+            }
+            representation+="|\n";
+        }
+        return representation;
     }
 
     @Override
     public String toString() {
-        return "";
+        return plateauToString(this.plateau);
     }
 
 }
--- src/test/java/org/epita/tp/serie2/TestsAlgoNaif.java
+++ src/test/java/org/epita/tp/serie2/TestsAlgoNaif.java
@@ -1,49 +1,177 @@
 package org.epita.tp.serie2;
 
+import org.assertj.core.api.Assertions;
+import org.epita.tp.serie2.naif.AlgoNaif;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.epita.tp.serie2.naif.AlgoNaif.copierPlateau;
+import static org.epita.tp.serie2.naif.AlgoNaif.plateauToString;
 
 public class TestsAlgoNaif {
 
+    private static final boolean[][] PLATEAU_TEST =
+            {
+                    {false, true, true, false},
+                    {false, false, true, false},
+                    {true, false, false, false},
+                    {true, false, false, true},
+            };
+    private AlgoNaif algoNaifTest = new AlgoNaif(PLATEAU_TEST);
+
     @Test
+    @DisplayName("copierPlateau fait une \"copie profonde\"")
     public void test_exercice2_1_1(){
-        //Copie
+        assertThat(copierPlateau(PLATEAU_TEST)).isNotSameAs(PLATEAU_TEST);
+        assertThat(Arrays.deepEquals(copierPlateau(PLATEAU_TEST), PLATEAU_TEST)).isTrue();
     }
 
     @Test
     public void test_exercice2_1_2(){
-        //Affichage
+        assertThat(plateauToString(PLATEAU_TEST)).isEqualTo("| |X|X| |\n| | |X| |\n|X| | | |\n|X| | |X|\n");
     }
 
     @Test
     public void test_exercice2_2_1(){
-        //Ligne
+        assertThat(algoNaifTest.verifierLigne(0)).isFalse();
+        assertThat(algoNaifTest.verifierLigne(1)).isTrue();
+        assertThat(algoNaifTest.verifierLigne(2)).isTrue();
+        assertThat(algoNaifTest.verifierLigne(3)).isFalse();
     }
 
     @Test
     public void test_exercice2_2_2(){
-        //Colonne
+        assertThat(algoNaifTest.verifierColonne(0)).isFalse();
+        assertThat(algoNaifTest.verifierColonne(1)).isTrue();
+        assertThat(algoNaifTest.verifierColonne(2)).isFalse();
+        assertThat(algoNaifTest.verifierColonne(3)).isTrue();
     }
 
     @Test
     public void test_exercice2_2_3(){
-        //Diagonale gauche
+        assertThat(algoNaifTest.verifierDiagonaleGauche(0)).isTrue();
+        assertThat(algoNaifTest.verifierDiagonaleGauche(1)).isTrue();
+        assertThat(algoNaifTest.verifierDiagonaleGauche(2)).isFalse();
+        assertThat(algoNaifTest.verifierDiagonaleGauche(3)).isFalse();
     }
 
     @Test
     public void test_exercice2_2_4(){
-        //Diagonale droite
+        assertThat(algoNaifTest.verifierDiagonaleDroite(0)).isTrue();
+        assertThat(algoNaifTest.verifierDiagonaleDroite(1)).isFalse();
+        assertThat(algoNaifTest.verifierDiagonaleDroite(2)).isTrue();
+        assertThat(algoNaifTest.verifierDiagonaleDroite(3)).isTrue();
     }
 
     @Test
     public void test_exercice2_3(){
+        assertThat(algoNaifTest.estUneSolution()).isFalse();
+
+        boolean[][] PLATEAU_TEST_OK =
+                {
+                        {true, false, false, false},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                };
+        AlgoNaif algoNaifTestOk = new AlgoNaif(PLATEAU_TEST_OK);
+
+        assertThat(algoNaifTestOk.estUneSolution()).isTrue();
 
     }
 
     @Test
-    public void test_exercice2_4(){
-
+    public void test_exercice2_4() throws Exception {
+        boolean[][] PLATEAU_DEPART_AVANT =
+                {
+                        {true, true, true, true},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                };
+        boolean[][] PLATEAU_DEPART_APRES =
+                {
+                        {false, true, true, true},
+                        {true, false, false, false},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                };
+        AlgoNaif algoNaif = new AlgoNaif(PLATEAU_DEPART_AVANT);
+        algoNaif.coupSuivant();
+        assertThat(Arrays.deepEquals(algoNaif.getPlateau(), PLATEAU_DEPART_APRES)).isTrue();
     }
+
+    @Test
+    public void test_exercice2_4_2() throws Exception {
+        boolean[][] PLATEAU_DEPART_AVANT =
+                {
+                        {false, true, true, true},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                        {true, false, false, false},
+                };
+        boolean[][] PLATEAU_DEPART_APRES =
+                {
+                        {true, false, true, true},
+                        {false, true, false, false},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                };
+        AlgoNaif algoNaif = new AlgoNaif(PLATEAU_DEPART_AVANT);
+        algoNaif.coupSuivant();
+        assertThat(Arrays.deepEquals(algoNaif.getPlateau(), PLATEAU_DEPART_APRES)).isTrue();
+    }
+
+    @Test
+    public void test_exercice2_4_3() throws Exception {
+        boolean[][] PLATEAU_DEPART_AVANT =
+                {
+                        {false, false, true, true},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                        {true, true, false, false},
+                };
+        boolean[][] PLATEAU_DEPART_APRES =
+                {
+                        {true, true, false, true},
+                        {false, false, true, false},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                };
+        AlgoNaif algoNaif = new AlgoNaif(PLATEAU_DEPART_AVANT);
+        algoNaif.coupSuivant();
+        assertThat(Arrays.deepEquals(algoNaif.getPlateau(), PLATEAU_DEPART_APRES)).isTrue();
+    }
+
+    @Test
+    public void test_exercice2_4_4() {
+        boolean[][] PLATEAU_DEPART_AVANT =
+                {
+                        {false, false, false, false},
+                        {false, false, false, false},
+                        {false, false, false, false},
+                        {true, true, true, true},
+                };
+        AlgoNaif algoNaif = new AlgoNaif(PLATEAU_DEPART_AVANT);
+        Assertions.assertThatThrownBy(() -> algoNaif.coupSuivant())
+                .hasMessageContaining("Vous avez épuisé toutes les possibilités");
+    }
+
+    @Test
+    public void test_exercice2_4_5() {
+        AlgoNaif algoNaif4x4 = new AlgoNaif(4);
+        List<boolean[][]> solutions4x4 = algoNaif4x4.listerSolutions();
+        assertThat(solutions4x4.size()).isEqualTo(2);
+
+        AlgoNaif algoNaif8x8 = new AlgoNaif(8);
+        List<boolean[][]> solutions8x8 = algoNaif8x8.listerSolutions();
+        assertThat(solutions8x8.size()).isEqualTo(92);
+    }
+
+
 
 }
